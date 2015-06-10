d5444746dcdc14a137e9d90bbe43b60688031fb1
Roshan Mahanama <rmtreks@gmail.com>
2015-06-10 23:39:39 +1000

    search bar

--- Pintu/Pintu/SearchVC.swift
+++ Pintu/Pintu/SearchVC.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class SearchVC: UIViewController, UISearchBarDelegate, UISearchDisplayDelegate {
 
+    @IBOutlet var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.searchBar.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +23,46 @@ class SearchVC: UIViewController, UISearchBarDelegate, UISearchDisplayDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        println("text did begin editing")
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        println("did end editing")
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        println("cancel button clicked")
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println("search button clicked")
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+      println("text changed")
+    }
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell;
+        
+        
+        return cell;
     }
     
 
