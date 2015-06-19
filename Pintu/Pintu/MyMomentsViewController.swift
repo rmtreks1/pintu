814bd15afd1eef23bda9b0625462d68fd91c9fb2
Roshan Mahanama <rmtreks@gmail.com>
2015-06-19 23:18:00 +1000

    loading up more inspirational pictures

---
+++ Pintu/Pintu/MyMomentsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyMomentsViewController.swift
+//  Pintu
+//
+//  Created by Roshan Mahanama on 19/06/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+import UIKit
+
+class MyMomentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
